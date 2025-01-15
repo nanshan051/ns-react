a4deeb72579960d55cc61ec808390364593fa58b
nanshan051 <nanshan_051@163.com>
2025-01-15 09:49:07 +0800

    feature: notes add 2.1；

--- docs/.vuepress/utils/navSidebar/notes/index.js
+++ docs/.vuepress/utils/navSidebar/notes/index.js
@@ -5,13 +5,25 @@ const notesSidebar = [
     collapsable: false,
   },
   {
-    title: "鉴权",
+    title: "1. 鉴权",
     path: "/notes/chapter-1/section-1",
     collapsable: false,
     children: [
       {
-        title: "Digest Auth",
+        title: "1.1. Digest Auth",
         path: "/notes/chapter-1/section-1",
+        collapsable: false,
+      },
+    ],
+  },
+  {
+    title: "2. 模块化",
+    path: "/notes/chapter-2/section-1",
+    collapsable: false,
+    children: [
+      {
+        title: "2.1. 介绍",
+        path: "/notes/chapter-2/section-1",
         collapsable: false,
       },
     ],
